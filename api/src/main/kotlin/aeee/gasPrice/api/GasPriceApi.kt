c3c9548a363880c5d7a6a29c14d2318f2a36d2aa
seoyeon <sylee@tanagement.net>
2021-07-06 17:03:30 +0900

    added git configuration and refreshScope

--- api/src/main/kotlin/aeee/gasPrice/api/GasPriceApi.kt
+++ api/src/main/kotlin/aeee/gasPrice/api/GasPriceApi.kt
@@ -2,10 +2,12 @@ package aeee.gasPrice.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 
 @SpringBootApplication(
     scanBasePackages = ["aeee.gasPrice"]
 )
+@RefreshScope
 class GasPriceApi
 
 fun main(args: Array<String>){
